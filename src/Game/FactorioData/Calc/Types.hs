9453a45a4abe6ba5e0c664a93b2d108a50df0f56
Will J <johns.willj@gmail.com>
2019-07-09 15:39:08 -0700

    reinit with stack and recursion schemes

--- src/Game/FactorioData/Calc/Types.hs
+++ src/Game/FactorioData/Calc/Types.hs
@@ -1,122 +1,45 @@
 module Game.FactorioData.Calc.Types where
 
 import Data.Default
-import Data.HashMap.Lazy (HashMap)
+import Data.Text
 
 import Game.FactorioData.Types
 
-data ProductionRateAnalysisRequest = ProductionRateAnalysisRequest
-  { prItems   :: HashMap String Double
-  , prOptions :: ProductionOptions
-  }
+data FactoryConfig = FactoryConfig  { technologyLevel :: TechnologyLevel
+                                    , oilProcessingConfig :: OilProcessingConfiguration
+                                    }
 
-data ProductionOptions = ProductionOptions
-  { poAssemblerConfiguration      :: AssemblerConfiguration
-  , poOilProcessingConfiguration  :: OilProcessingConfiguration
-  , poPowerPlantConfiguration     :: PowerPlantConfiguration
+instance Default FactoryConfig where def = FactoryConfig def def
 
-    -- | Module assignment rules; each one is applied until no more compatible
-    -- module slots are available before moving on to the next one.
-  , poModuleConfiguration         :: [ModuleConfiguration]
-  }
+data TechnologyEpoch  = Beginning
+                      -- ^ All techs that can be unlocked by red science only
+                      | Early
+                      -- ^ RG techs only
+                      | Middle
+                      -- ^ RGB techs only
+                      | Advanced
+                      -- ^ RGBYP techs only
+                      | Late
+                      -- ^ All non-infinite techs
 
-instance Default ProductionOptions where
-  def = ProductionOptions def def def def
+-- | All techs up to and including the given epoch, plus more
+data TechnologyLevel  = TechnologyLevel TechnologyEpoch [Technology]
 
-data AssemblerConfiguration = AssemblerConfiguration
-  { -- | Maximum allowed assembler level. Report will fail if any precursor
-    -- recipes require more advanced assemblers.
-    acMaxLevel :: AssemblerLevel
+data Technology = BasicTech Text | InfiniteTech Text Int
 
-    -- | Minimum allowed assembler level. If not set, recipes will be crafted
-    -- in the highest-level assembler (but not higher than 'acMaxLevel').
-  , acMinLevel :: Maybe AssemblerLevel
-  }
+instance Default TechnologyLevel where def = TechnologyLevel Advanced []
 
-instance Default AssemblerConfiguration where
-  def = AssemblerConfiguration AssemblerThree def
+-- | What level of oil refining recipes are available?
+-- Coal liquefaction must be configured like e.g. "refine up to 1000 crude oil per minute,
+-- then use coal liquefaction for any leftover"
+data OilProcessingConfiguration = BasicOil | AdvancedOil | CoalLiquefaction LiquefactionScheme RateUnit Int
 
-data OilProcessingConfiguration = OilProcessingConfiguration
-  { -- | Whether or not the Advanced Oil Processing recipe should be used.
-    opcAdvancedProcessingEnabled  :: Bool
+-- | Whether to base calculated coal liquefaction setup on a limited crude consumption vs. limited coal consumption
+data LiquefactionScheme = LimitedCoal | LimitedCrude
 
-    -- | Strategy to use for balancing crude / coal liquefaction
-    opcRawMaterialStrategy        :: OilProcessingRawMaterialStrategy
+-- | One "tick" is the base unit of game time. There are 60 ticks in a game second. One game second should be
+-- equal to one second in the real world, unless the game speed is artificially slowed down by mods or by CPU
+-- performance issues in very large factories.
+data RateUnit = PerTick | PerGameSecond | PerGameMinute
 
-    -- | Strategy to use for cracking surplus heavy oil fractions
-    opcCrackingStrategy           :: OilProcessingCrackingStrategy
-  }
-
--- | How to balance crude refining vs. coal liquefaction. Default is to
--- use only crude oil, and as much as is needed.
-data OilProcessingRawMaterialStrategy = OilProcessingRawMaterialStrategy
-  { -- | Whether to prioritize use of crude oil or coal liquefaction
-    opcRawPriority    :: OilProcessingRawPriority
-
-    -- | Maximum crude oil yield to pump before starting to liquefy coal
-    opcMaxCrudeYield  :: Maybe Int
-
-    -- | Maximum coal per second to liquefy before starting to refine crude
-    opcMaxCoalRate    :: Maybe Double
-  }
-
-data OilProcessingRawPriority
-  = CrudeFirst  -- ^ Use crude oil up to opcMaxCrudeYield, then use coal
-  | CoalFirst   -- ^ Use coal liquefaction up to opcMaxCoalRate, then crude
-
--- | How to crack excess heavy oil fractions, if present. Default is to
--- perform no cracking.
-data OilProcessingCrackingStrategy
-  = NoCracking
-
-  -- | Excess heavy fractions will be cracked so that surplus fractions are
-  -- generated at the same rate, without rebalancing raw material consumption.
-  | BalancedSurplus
-
-  -- | Excess heavy fractions will be cracked until they fall below the
-  -- specified ceilings. If a ceiling is set for petroleum, raw material
-  -- consumption will be rebalanced if cracking the heavier fractions would
-  -- push it above that ceiling.
-  | SurplusCeilings (Maybe Double) (Maybe Double) (Maybe Double)
-
-  -- | Excess heavy fractions will be cracked, and raw material consumption
-  -- rebalanced, to minimize surplus of all oil fractions.
-  | MinimizeSurplus
-
-instance Default OilProcessingConfiguration where
-  def = OilProcessingConfiguration True def def
-
-instance Default OilProcessingRawMaterialStrategy where
-  def = OilProcessingRawMaterialStrategy def def def
-
-instance Default OilProcessingRawPriority where
-  def = CrudeFirst
-
-instance Default OilProcessingCrackingStrategy where
-  def = NoCracking
-
-data ModuleConfiguration = ModuleConfiguration
-  { -- | Which module to use
-    mcModuleName :: String
-
-    -- | Maximum number of modules to apply, in each matching machine.
-    -- If unset, all remaining compatible slots will be filled.
-  , mcModuleCountPerMachine :: Maybe Int
-
-    -- | Maximum number of modules to apply for this rule across the entire
-    -- factory. If unset, all compatible slots will be filled before
-    -- moving on to the next rule.
-  , mcTotalModuleCount      :: Maybe Int
-
-    -- | How to select the machines which will get these modules. Defaults
-    -- to 'HighestProduction' (for productivity modules); 'HighestPower'
-    -- (for efficiency modules); or 'HighestEffort' (for speed modules).
-  , mcModuleTarget :: ModuleTarget
-  }
-
-data ModuleTarget
-  = HighestProduction
-  | HighestPower
-  | HighestEffort
-  | SpecificRecipe String
-    -- ^ This module will go to machines running the specified recipe.
+instance Default OilProcessingConfiguration where def = AdvancedOil
--- src/Game/FactorioData/Types.hs
+++ src/Game/FactorioData/Types.hs
@@ -1,7 +1,47 @@
 module Game.FactorioData.Types where
 
-data AssemblerLevel = AssemblerOne | AssemblerTwo | AssemblerThree
+import Data.Functor.Foldable
+import Data.Text
 
+data FactoryF f = Extractor Drill Resource
+                -- ^ an item source (drills, pumpjacks, ...)
 
+                | Consumer ItemConsumer
+                -- ^ an item sink (labs, rocket silo, weapons)
 
+                | Crafting Crafter Recipe
+                -- ^ something that consumes items to create different ones (assemblers, chemical plants, ...)
 
+                | Transport Logistic
+                -- ^ a process that moves items from one place to another, so that the inputs == outputs
+
+                | Parallel f [f]
+                -- ^ multiple subfactories (e.g. parallel belts/assemblers) whose inputs/outputs are not connected
+                -- to each other (but possibly shared, e.g. by one belt)
+
+                | Join f f
+                -- ^ one subfactory feeds into another, making its outputs available as inputs to the next
+
+-- | normal resources like iron ore vs. one with yield mechanics like crude oil
+data Resource = Basic ResourceName | Yield ResourceName Int
+
+-- | Type synonyms for entity names
+-- todo: expand some of these to support modules
+type Drill = Text
+type ResourceName = Text
+type ItemConsumer = Text
+type Crafter = Text
+type Recipe = Text
+
+data BeltLevel = Normal | Fast | Express
+data Logistic = Belt BeltLevel | LogisticBot | Train
+
+instance Functor FactoryF where
+  fmap t (Parallel f fs)  = Parallel (t f) (t <$> fs)
+  fmap t (Join f1 f2)     = Join (t f1) (t f2)
+  fmap _ (Extractor d r)  = Extractor d r
+  fmap _ (Consumer c)     = Consumer c
+  fmap _ (Crafting c r)   = Crafting c r
+  fmap _ (Transport l)    = Transport l
+
+type Factory = Fix FactoryF
